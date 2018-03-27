041b2cb44487d9f791574a8eccfe64a3b671ce41
10537 <yosefeliyahu@gmail.com>
2018-03-27 01:33:27 -0500

    [IMP]Finish RestAPI.

--- happypets_prototype/api/tests.py
+++ happypets_prototype/api/tests.py
@@ -36,3 +36,29 @@ class ViewTestCase(TestCase):
     def test_api_can_create_a_bucketlist(self):
         """Test the api has bucket creation capability"""
         self.assertEqual(self.response.status_code, status.HTTP_201_CREATED)
+
+    def test_api_can_get_a_bucketlist(self):
+        """Test api can get a given bucketlist"""
+        bucketlist = Bucketlist.objects.get()
+        response = self.client.get(reverse('details', kwargs={'pk': bucketlist.id}), format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertContains(response, bucketlist)
+
+    def test_api_can_update_bucketlist(self):
+        """Test api can update a given bucketlist"""
+        bucketlist = Bucketlist.objects.get()
+        change_buckelist = {'name': 'something new'}
+        response = self.client.put(reverse('details', kwargs={'pk': bucketlist.id}), 
+        change_buckelist, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_api_can_delete_bucketlist(self):
+        """Test api can delete a given buckelist"""
+        bucketlist = Bucketlist.objects.get()
+        response = self.client.delete(
+            reverse('details', kwargs={'pk': bucketlist.id}), format='json', follow=True
+        )
+
+        self.assertEquals(response.status_code, status.HTTP_204_NO_CONTENT)
